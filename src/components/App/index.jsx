a2153e00f830fd5c955a3987c455e50a16986cf4
Onyx <onyxganda@gmail.com>
2022-07-13 13:01:33 +0000

    Added text file upload, check and preview

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,80 +1,100 @@
 import React, { Component } from "react";
-import './index.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import GridManagement from "../../utils/gridManagement/index.tsx";
-import Grid from "../Grid"
-import ActionBar from "../ActionBar"
+import Grid from "../Grid";
+import ActionBar from "../ActionBar";
 
-  const View = ({prevPopulation, population, fileFn, calcFn}) => {
-    
-    return <div>
+const View = ({ prevPopulation, population, fileFn, calcFn, preview }) => {
+  console.log("View - got preview", preview);
+  return (
+    <div>
       <Grid id="prev" population={prevPopulation} />
-      <hr/>
+      <hr />
       <Grid id="current" population={population} />
-      <ActionBar uploadFile={fileFn} calcNextGen={calcFn} />
+      <ActionBar
+        textPreview={preview}
+        uploadFile={fileFn}
+        calcNextGen={calcFn}
+      />
     </div>
-      
-  }
+  );
+};
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       population: [],
-      prevPopulation: []
-    }
+      prevPopulation: [],
+      textPreview: "test",
+    };
   }
-grid = new GridManagement([4,8], "********"+
-                                                    "**##****"+
-                                                    "*###****"+
-                                                    "********",'*', '#')
-    //grid.calcNextGeneration();
+  grid = new GridManagement(
+    [4, 8],
+    "********" + "**##****" + "*###****" + "********",
+    "*",
+    "#"
+  );
+  //grid.calcNextGeneration();
 
-  componentDidMount()   {
+  componentDidMount() {
     console.log("Called componentDidMount");
-    var population = [... this.grid.getPopulation()];
+    var population = [...this.grid.getPopulation()];
     var prevPopulation = JSON.parse(JSON.stringify(population));
-    this.setState({ 
+    this.setState({
       population: population,
-      prevPopulation: prevPopulation
-    })
+      prevPopulation: prevPopulation,
+    });
   }
 
   calculateNextGen() {
     console.log("App - calling calculate next gen");
-    var prevPopulation  = JSON.parse(JSON.stringify(this.state.population));
+    var prevPopulation = JSON.parse(JSON.stringify(this.state.population));
     console.log("App - Got prev population", prevPopulation);
     this.grid.calcNextGeneration();
-    var population = [... this.grid.getPopulation()];
+    var population = [...this.grid.getPopulation()];
     console.log("App - Got next population", population);
-    this.setState( (state) => ({ 
-        prevPopulation: prevPopulation,
-        population: population
-      })
-    );
-   
+    this.setState((state) => ({
+      prevPopulation: prevPopulation,
+      population: population,
+    }));
   }
 
-  uploadFile() {
-    console.log("App - Uploading file..");
+  readFile(e) {
+    console.log("Got text", e.target.result);
+    if (e.target.result) {
+      this.setState({ textPreview: e.target.result });
+    }
   }
-  
+
+  uploadFile(value) {
+    console.log("App - Uploading file..", value);
+    if (value) {
+      var fileR = new FileReader();
+      fileR.addEventListener("load", (e) => this.readFile(e));
+      fileR.readAsText(value.files[0]);
+    }
+  }
+
   render() {
     return (
       <Router>
         <Routes>
-          <Route exact path="/" 
+          <Route
+            exact
+            path="/"
             element={
-              <View 
-                calcFn={() => this.calculateNextGen()} 
-                fileFn={() => this.uploadFile()}
-                prevPopulation={this.state.prevPopulation} 
-                population={this.state.population} />}
-          />;
+              <View
+                calcFn={() => this.calculateNextGen()}
+                fileFn={(value) => this.uploadFile(value)}
+                prevPopulation={this.state.prevPopulation}
+                population={this.state.population}
+                preview={this.state.textPreview}
+              />
+            }
+          />
+          ;
         </Routes>
       </Router>
     );
